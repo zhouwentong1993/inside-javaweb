7f1a0f4dd4db11afc7bd29f4aeca26f5c905ea94
zhouwentong <zhouwentong1@gmail.com>
2016-04-03 00:04:15 +0800

    测试通过 get 提交和 post 提交。

--- src/test/java/TestHttp.java
+++ src/test/java/TestHttp.java
@@ -1,4 +1,8 @@
+import com.puhui.utils.HttpUtils;
+import net.sf.json.JSONObject;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * Created by wentong on 2016/3/29.
@@ -6,7 +10,16 @@ import org.junit.Test;
  */
 public class TestHttp {
     @Test
-    public void test() {
+    public void testGet() throws IOException {
+        String url = "http://localhost:8080/test/testGet?username=zhangsan";
+        HttpUtils.sendGet(url);
+    }
 
+    @Test
+    public void testPost() throws IOException {
+        String url = "http://localhost:8080/test/testPost";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", "周文童");
+        HttpUtils.sendPost(url,jsonObject.toString());
     }
 }
