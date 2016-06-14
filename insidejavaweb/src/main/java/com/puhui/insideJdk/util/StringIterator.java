34641fc91f9fb1bceb0cd730da132754ba6eeba8
zhouwentong <zhouwentong1@gmail.com>
2016-06-14 18:50:24 +0800

    在 Java 中 String 类型不能用于 foreach 循环，但是在 Python 中是可以的。

---
+++ insidejavaweb/src/main/java/com/puhui/insideJdk/util/StringIterator.java
@@ -0,0 +1,14 @@
+package com.puhui.insideJdk.util;
+
+/**
+ * Created by wentong on 2016/6/14.
+ * 在 Java 中 String 不能用 foreach 循环
+ * 但是在 Python 中是可以的。
+ */
+
+public class StringIterator {
+    public static void main(String[] args) {
+        String s = "abcdef";
+
+    }
+}
