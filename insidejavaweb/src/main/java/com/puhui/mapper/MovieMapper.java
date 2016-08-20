8905617bb6eef1368aa7dcba6c1ce1e7cbc41090
zhouwentong <zhouwentong1@gmail.com>
2016-08-20 22:56:37 +0800

    测试，但是由于 tkMybatis 出现问题，所以只能暂时将配置文件回滚到原先的模式。

--- insidejavaweb/src/main/java/com/puhui/mapper/MovieMapper.java
+++ insidejavaweb/src/main/java/com/puhui/mapper/MovieMapper.java
@@ -1,11 +1,13 @@
 package com.puhui.mapper;
 
 import com.puhui.jsoup.vo.DouBanVo;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by wentong on 2016/4/4.
  * 豆瓣电影 Mapper 接口
  */
+@Repository
 public interface MovieMapper {
     void insertMovie(DouBanVo douBanVo);
 }
