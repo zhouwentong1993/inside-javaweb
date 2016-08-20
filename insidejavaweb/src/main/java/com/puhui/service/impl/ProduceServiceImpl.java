37c53c3d9f60cf1757fb0f37cfe73f83b3ccc154
zhouwentong <zhouwentong1@gmail.com>
2016-08-20 23:00:22 +0800

    完成环境搭建，单元测试能够成功跑起来了。待做：控制时间

--- insidejavaweb/src/main/java/com/puhui/service/impl/ProduceServiceImpl.java
+++ insidejavaweb/src/main/java/com/puhui/service/impl/ProduceServiceImpl.java
@@ -2,6 +2,7 @@ package com.puhui.service.impl;
 
 import com.puhui.service.ProduceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ import javax.jms.Destination;
 @Service
 public class ProduceServiceImpl implements ProduceService {
     @Autowired
+    @Qualifier("jmsTemplate")
     private JmsTemplate jmsTemplate;
 
     @Override
