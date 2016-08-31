321932f9640ec87fc264f14b28564bf9b264f390
zhouwentong <zhouwentong1@gmail.com>
2016-08-31 23:51:54 +0800

    打算通过 Spring MVC 传递 List

--- insidejavaweb/src/main/java/com/puhui/web/controller/UserController.java
+++ insidejavaweb/src/main/java/com/puhui/web/controller/UserController.java
@@ -3,9 +3,15 @@ package com.puhui.web.controller;
 import com.puhui.mapper.UserMapper;
 import com.puhui.vo.User;
 import com.puhui.vo.UserInfo;
+import com.puhui.vo.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by wentong on 2016/8/28.
@@ -28,6 +34,13 @@ public class UserController {
         userMapper.saveUser(user);
         System.out.println(user);
         System.out.println(userInfo);
+        return null;
+    }
+
+    @RequestMapping(value = "getValue",method = RequestMethod.POST)
+    @ResponseBody
+    public Object getValue(@RequestBody List<Value> valueList) {
+        System.out.println(valueList);
         return null;
     }
 }
