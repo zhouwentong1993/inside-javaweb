e1e3ac84ddf6c9ab26c06fbd6604c9c4b15fe251
zhouwentong <zhouwentong1@gmail.com>
2016-09-09 00:28:12 +0800

    修改问题，最近需要将 Spring Boot 相关整理一下了

--- restful/src/main/java/com/puhui/demo/Application.java
+++ restful/src/main/java/com/puhui/demo/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -14,6 +15,19 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+        ConfigurableApplicationContext context = null;
+        try
+        {
+            context = SpringApplication.run(Application.class, args);
+        }
+        finally
+        {
+            if (context != null) {
+                context.close();
+            }
+        }
     }
+
+
 }
